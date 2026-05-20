b6af160178891466b16407fe3163e8e4bf0ac882
Sarah Schulte <git@sarah.engineer>
2026-05-20 17:44:38 -0400

    this one for sure

--- addons/rcade/rcade_input.gd
+++ addons/rcade/rcade_input.gd
@@ -1,12 +1,16 @@
 extends Node
 
+var cb
 func _ready() -> void:
+	call_deferred("setup")
+
+func setup():
 	if OS.has_feature("rcade"):
-		var cb = JavaScriptBridge.create_callback(on_event)
+		cb = JavaScriptBridge.create_callback(on_event)
 		var rcade = JavaScriptBridge.get_interface("RCadeInput")
 		rcade.register(cb)
 
-signal event(data: JavaScriptObject)
+signal event(data)
 
 func on_event(args: Array):
 	event.emit(args[0].data)
--- scripts/debug_events.gd
+++ scripts/debug_events.gd
@@ -1,11 +1,13 @@
 extends Control
 
 
+var jsJSON
 func _ready() -> void:
 	RCadeInput.event.connect(on_event)
+	jsJSON = JavaScriptBridge.get_interface("JSON")
 
-func on_event(data: JavaScriptObject):
+func on_event(data):
 	var l = Label.new()
-	l.text = data.to_string();
+	l.text = jsJSON.stringify(data)
 	$VBoxContainer.add_child(l)
 	$VBoxContainer.move_child(l, 0)
